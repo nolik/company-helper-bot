b953afdff869f9db1f4a3451f127d37c943406a5
Ihar Novik <i.novik@godeltech.com>
2021-11-05 10:05:07 +0300

    Add commands support.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
+use std::error::Error;
 
-use teloxide::prelude::*;
+use teloxide::{prelude::*, utils::command::BotCommand};
 
 #[tokio::main]
 async fn main() {
@@ -12,9 +13,38 @@ async fn run() {
     log::info!("Starting bot...");
 
     let bot = Bot::from_env().auto_send();
-    teloxide::repl(bot, move |_message| async move {
-        log::info!("handle message");
-        respond(())
-    })
-    .await;
+    teloxide::commands_repl(bot, "godel", answer).await;
+}
+
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+async fn answer(
+    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => cx.answer(Command::descriptions()).await?,
+        Command::Username(username) => {
+            cx.answer(format!("Your username is @{}.", username))
+                .await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            cx.answer(format!(
+                "Your username is @{} and age is {}.",
+                username, age
+            ))
+            .await?
+        }
+    };
+
+    Ok(())
 }
